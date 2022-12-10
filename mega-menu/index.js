66ff66d5dc5339f365fedecc7bd7d2784b998869
Jake Stone <Stonedev12@gmail.com>
2022-12-09 23:10:00 -0800

    The mega menu now stays open when expected and closes when expected.

--- mega-menu/index.js
+++ mega-menu/index.js
@@ -2,12 +2,20 @@ var megaMenu = document.getElementById('mega');
 var links = document.getElementById('nav');
 
 links.addEventListener('mouseover', function(event){
-  if (event.target.className === 'link'){
+  if (event.target.className === 'link') {
     megaMenu.style.display = 'flex';
   }
 })
-links.addEventListener('mouseout', function(event){
+links.addEventListener('mouseover', function (event) {
+  if (event.target.className === 'nav-links') {
+    megaMenu.style.display = 'none';
+  }
+})
+mega.addEventListener('mouseout', function(event){
   megaMenu.style.display = 'none';
   megaMenu.style.opacity = '1';
   megaMenu.style.transitionDelay = '0.5s';
+})
+mega.addEventListener('mouseover', function (event) {
+    megaMenu.style.display = 'flex';
 })
