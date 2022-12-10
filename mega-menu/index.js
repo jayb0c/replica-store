b3ec7b4d4386a0ad0b9c42416084ae480c3746e7
Jake Stone <Stonedev12@gmail.com>
2022-12-10 00:15:47 -0800

    Site styles have been updated to reflect color scheme and featured product/ links have been added to the mega menu.

--- mega-menu/index.js
+++ mega-menu/index.js
@@ -4,18 +4,24 @@ var links = document.getElementById('nav');
 links.addEventListener('mouseover', function(event){
   if (event.target.className === 'link') {
     megaMenu.style.display = 'flex';
+    megaMenu.style.opacity = '1';
+    megaMenu.style.transitionDelay = '2s';
   }
 })
 links.addEventListener('mouseover', function (event) {
   if (event.target.className === 'nav-links') {
     megaMenu.style.display = 'none';
+    megaMenu.style.opacity = '1';
+    megaMenu.style.transitionDelay = '2s';
   }
 })
 mega.addEventListener('mouseout', function(event){
   megaMenu.style.display = 'none';
   megaMenu.style.opacity = '1';
-  megaMenu.style.transitionDelay = '0.5s';
+  megaMenu.style.transitionDelay = '2s';
 })
 mega.addEventListener('mouseover', function (event) {
     megaMenu.style.display = 'flex';
+  megaMenu.style.opacity = '1';
+  megaMenu.style.transitionDelay = '2s';
 })
