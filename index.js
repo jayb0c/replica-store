9edca619816b1c5a4f2cc0997487f19fb15d3e94
Jake Stone <Stonedev12@gmail.com>
2022-12-19 22:59:20 -0800

    Buttons now have a max-width of 400px because they were becoming too wide at large tablet widths.

--- index.js
+++ index.js
@@ -137,15 +137,15 @@ fetch('https://api.escuelajs.co/api/v1/products')
       if (newData[i].images != '') {
       let prod = document.createElement('div');
       prod.className = 'slide';
-        prod.style.backgroundImage = 'url(' + newData[i].images + ')';
-        let cover = document.createElement('div');
-        cover.className = 'slide-cover';
-        let span = document.createElement('span');
-        span.textContent = '$' + newData[i].price;
-        span.className = 'cover-copy';
-        cover.appendChild(span);
-        prod.appendChild(cover);
-        firstCollectionViewport.appendChild(prod);
+      prod.style.backgroundImage = 'url(' + newData[i].images + ')';
+      let cover = document.createElement('div');
+      cover.className = 'slide-cover';
+      let span = document.createElement('span');
+      span.textContent = '$' + newData[i].price;
+      span.className = 'cover-copy';
+      cover.appendChild(span);
+      prod.appendChild(cover);
+      firstCollectionViewport.appendChild(prod);
       }
     }
 
@@ -154,12 +154,12 @@ fetch('https://api.escuelajs.co/api/v1/products')
       if (newData[i].images != '') {
       let prod = document.createElement('div');
       prod.className = 'slide';
-        prod.style.backgroundImage = 'url(' + newData[i].images + ')';
+      prod.style.backgroundImage = 'url(' + newData[i].images + ')';
       let cover = document.createElement('div');
       cover.className = 'slide-cover';
       let span = document.createElement('span');
       span.textContent = '$' + newData[i].price;
-       span.className = 'cover-copy';
+      span.className = 'cover-copy';
       cover.appendChild(span);
       prod.appendChild(cover);
       secondCollectionViewport.appendChild(prod);
@@ -167,36 +167,36 @@ fetch('https://api.escuelajs.co/api/v1/products')
     }
   });
 
-  //FEATURED PRODUCT API CALL
-  fetch('https://api.escuelajs.co/api/v1/products')
-    .then(res => res.json())
-    .then(data => {
-      obj = data;
-    })
-    .then(() => {
-      let thisData = [];
-      for (let y = 0; y < data.length; y++) {
-        if (data[y].category.id === 4) {
-          thisData.push(data[y]);
-        }
+//FEATURED PRODUCT API CALL
+fetch('https://api.escuelajs.co/api/v1/products')
+  .then(res => res.json())
+  .then(data => {
+    obj = data;
+  })
+  .then(() => {
+    let thisData = [];
+    for (let y = 0; y < data.length; y++) {
+      if (data[y].category.id === 4) {
+        thisData.push(data[y]);
       }
+    }
 
-      /* THE FEATURED PRODUCT SECTION HAS A SINGLE PRODUCT PULLED AND THE SECTION'S CONTENT IS POPULATED FROM THIS PRODUCT'S DATA. */
-      let featuredImage = document.getElementById('image-block');
-      let featuredCopy = document.getElementById('copy-block');
+    /* THE FEATURED PRODUCT SECTION HAS A SINGLE PRODUCT PULLED AND THE SECTION'S CONTENT IS POPULATED FROM THIS PRODUCT'S DATA. */
+    let featuredImage = document.getElementById('image-block');
+    let featuredCopy = document.getElementById('copy-block');
 
-      let featuredHeader = document.createElement('h2');
-      featuredHeader.textContent = 'Speed Run 2';
-      let featuredPrice = document.createElement('h4');
-      featuredPrice.textContent = '$' + thisData[30].price;
-      let featuredBody = document.createElement('p');
-      featuredBody.textContent = 'The Speed Run 2 has the same supersoft feel that lets you feel the potential when your foot hits the pavement. We created the shoe with plenty of snappy responsiveness and incredible support to help keep you feeling secure and competitive. Its 1 of our most tested shoes, still designed for you to stay on the track and away from the sidelines.';
-      let featuredButton = document.createElement('button');
-      featuredButton.className = 'button';
-      featuredButton.textContent = 'Add to Cart';
+    let featuredHeader = document.createElement('h2');
+    featuredHeader.textContent = 'Speed Run 2';
+    let featuredPrice = document.createElement('h4');
+    featuredPrice.textContent = '$' + thisData[30].price;
+    let featuredBody = document.createElement('p');
+    featuredBody.textContent = 'The Speed Run 2 has the same supersoft feel that lets you feel the potential when your foot hits the pavement. We created the shoe with plenty of snappy responsiveness and incredible support to help keep you feeling secure and competitive. Its 1 of our most tested shoes, still designed for you to stay on the track and away from the sidelines.';
+    let featuredButton = document.createElement('button');
+    featuredButton.className = 'button';
+    featuredButton.textContent = 'Add to Cart';
 
-      featuredCopy.appendChild(featuredHeader);
-      featuredCopy.appendChild(featuredPrice);
-      featuredCopy.appendChild(featuredBody);
-      featuredCopy.appendChild(featuredButton);
-    });
+    featuredCopy.appendChild(featuredHeader);
+    featuredCopy.appendChild(featuredPrice);
+    featuredCopy.appendChild(featuredBody);
+    featuredCopy.appendChild(featuredButton);
+  });
