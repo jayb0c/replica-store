2288d6edd17f748cfb9dfc4f24a985724634e3e0
Jake Stone <Stonedev12@gmail.com>
2022-12-19 22:51:17 -0800

    JavaScript variables have been renamed to make the code more understandable. Also, code has been compiled. Unused code has been removed.Speed has noticeably increased.

--- index.js
+++ index.js
@@ -1,117 +1,202 @@
-//Mega menu functionality. Only links open it and when the mega menu is hovered it remains open.
-var megaMenu = document.getElementById('mega');
-var links = document.getElementById('nav');
-var cart = document.getElementById('cart');
-var cartIcon = document.getElementById('cartIcon');
-var cartX = document.getElementById('cartX');
-var mobile = false;
+/* MEGA MENU and CART VARIABLES */
+const megaMenu = document.getElementById('mega');
+const navLinks = document.getElementById('navLinks');
+const cartDrawer = document.getElementById('cartDrawer');
+const cartIcon = document.getElementById('cartIcon');
+const closeCart = document.getElementById('closeCart');
 
+/* IF MEGA MENU IS SCROLLED PAST, CLOSE MENU */
 if (window.scrollY > (megaMenu.offsetHeight + megaMenu.offsetTop)) {
   megaMenu.className = "mega-menu hide";
 }
-links.addEventListener('mouseover', function(event){
-  if(mobile === true){ return; }
+/* MEGA MENU OPENS WHEN 'SHOP' IS HOVERED */
+navLinks.addEventListener('mouseover', function(event){
   if (event.target.className === 'link desk-only' || event.target === megaMenu) {
-    megaMenu.style.display = 'flex';
     megaMenu.className = "mega-menu show";
-  }
-  else{
+  } else{
     megaMenu.className = "mega-menu hide";
   }
 })
-
-mega.addEventListener('mouseout', function(event){
+/* MEGA MENU CLOSES WHEN MOUSE LEAVES MENU */
+megaMenu.addEventListener('mouseout', function(event){
   megaMenu.className = "mega-menu hide";
 })
-mega.addEventListener('mouseover', function (event) {
+/* MEGA MENU REMAINS OPEN WHEN CURSOR IS OVER IT */
+megaMenu.addEventListener('mouseover', function (event) {
   megaMenu.className = "mega-menu show";
-  megaMenu.style.display = 'flex';
 })
-
-links.addEventListener('click', function (event) {
+/* CLICKING THE CART ICON OPENS THE CART DRAWER */
+navLinks.addEventListener('click', function (event) {
   if (event.target === cartIcon) {
-    cart.style.display = 'flex';
+    cartDrawer.style.display = 'flex';
   }
 })
-cart.addEventListener('click', function (event){
-  if(event.target === cartX) {
-    cart.style.display = 'none';
+/* IF THE 'X' IS CLICKED IN THE CART, THE CART CLOSES */
+cartDrawer.addEventListener('click', function (event){
+  if(event.target === closeCart) {
+    cartDrawer.style.display = 'none';
   }
 })
 
-const first = document.getElementById('collectionFirst');
-const second = document.getElementById('collectionSecond');
+/* FIRST AND SECOND COLLECTION CAROUSELS */
+const firstCollection = document.getElementById('collectionFirst');
+const secondCollection = document.getElementById('collectionSecond');
 
-scrollCarousel(first, '1');
-scrollCarousel(second, '2');
-function scrollCarousel (carousel, num){
-  var addON = 0;
-  var middles = document.getElementById('collection' + num);
+/* CAROUSEL EVENT LISTENERS ARE APPLIED TO BOTH COLLECTION CAROUSELS SEPARATELY */
+scrollCarousel(firstCollection, '1');
+scrollCarousel(secondCollection, '2');
 
-  carousel.addEventListener('click', function (event) {
-    let fullSet = middles.scrollWidth;
-    let middleSet = middles.offsetWidth;
-    let rightSet = middleSet + addON;
-    let leftSet = rightSet - middleSet;
-    let child = middles.firstElementChild.offsetWidth;
-    let save = 0;
 
-    if (rightSet > fullSet - child){
-      middles.classList.add('wrap1');
-      middles.scrollTo(0,0);
+/* THIS FUNCTION RESPONDS TO CAROUSEL CLICKS. IT BEGINS AT THE BEGINNING OF THE SCROLLBAR AND EACH CLICK IS SAVED IN A VARIABLE WHICH INCREASES. IF THE FULL LENGTH OF THE SCROLL BAR HAS BEEN REACHED, THE CAROUSEL SETS BACK TO THE BEGINNING WITH A CSS ANIMATION */
+
+function scrollCarousel (clickedCarousel, num){
+  let forwardScroll = 0;
+  let collectionCarousel = document.getElementById('collection' + num);
+
+  clickedCarousel.addEventListener('click', function (event) {
+    let carouselLength = collectionCarousel.scrollWidth;
+    let carouselViewport = collectionCarousel.offsetWidth;
+    let scrollLocation = carouselViewport + forwardScroll;
+    let carouselSlideSize = collectionCarousel.firstElementChild.offsetWidth;
+
+    if (scrollLocation > carouselLength - carouselSlideSize){
+      collectionCarousel.classList.add('wrap1');
+      collectionCarousel.scrollTo(0,0);
+
+      /* FOR BEST RESULTS, MAKE SURE THAT THE CSS CLASSES wrap1 AND wrap2 HAVE TRANSITIONS ARE LONGER THAN THE TIMEOUT TO PREVENT A GAP BETWEEN THEM. */
       setTimeout(function () {
-        middles.classList.add('wrap2');
-      }, 200);
-      addON = 0;
-    }
-    else{
-      middles.classList.remove('wrap2');
-      middles.classList.remove('wrap1');
+        collectionCarousel.classList.add('wrap2');
+      }, 300);
+      forwardScroll = 0;
+    } else{
+      collectionCarousel.classList.remove('wrap2');
+      collectionCarousel.classList.remove('wrap1');
       if (event.target.className === 'bookend-r') {
         event.target.previousElementSibling.scrollTo({
           top: 0,
-          left: rightSet,
+          left: scrollLocation,
           behavior: 'smooth'
         });
-        if (rightSet <= middles.scrollWidth) { addON = rightSet; };
+        if (scrollLocation <= collectionCarousel.scrollWidth) { forwardScroll = scrollLocation; };
       }
-
-
-      /*
-      if (event.target.className === 'bookend-l') {
-        if (addON == 0) {
-          middles.classList.add('wrap2');
-          middles.scrollTo(fullSet, 0);
-          setTimeout(function () {
-            middles.classList.add('wrap1');
-          }, 200);
-        } else{
-          event.target.nextElementSibling.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-          });
-          addON = 0;
-        }
-      }
-      */
     }
   });
 }
 
 /* MOBILE MENU OPENING */
-const lines = document.getElementById('mobile-menu');
+const hamburgerIcon = document.getElementById('mobile-menu');
 const mobileMenu = document.getElementById('mega-mobile');
-const mobileX = document.getElementById('mobile-x');
+const mobileCloseCart = document.getElementById('mobile-x');
 const page = document.getElementsByTagName('body')[0];
 
-lines.addEventListener('click', function(){
+/* WHEN THE MOBILE HAMBURGER MENU IS CLICKED, THE MOBILE MENU IS DISPLAYED FULLSCREEN */
+hamburgerIcon.addEventListener('click', function(){
   mobileMenu.style.display = 'flex';
   page.style.position = 'fixed';
   page.style.top = 0;
 })
-mobileX.addEventListener('click', function(){
+
+/* THE MOBILE CART CAN BE CLOSED BY CLICKING THE 'X' ICON */
+mobileCloseCart.addEventListener('click', function(){
   mobileMenu.style.display = 'none';
   page.style.position = 'relative';
   page.style.top = 'none';
 })
+
+
+
+/* THESE ARE THE VIEWPORTS OF EACH COLLECTION CAROUSEL */
+const firstCollectionViewport = document.getElementById('collection1');
+const secondCollectionViewport = document.getElementById('collection2');
+
+const getProducts = new XMLHttpRequest();
+getProducts.open('GET', 'https://api.escuelajs.co/api/v1/products');
+getProducts.send();
+getProducts.onload = function () {
+  data = JSON.parse(getProducts.response);
+}
+
+var obj;
+fetch('https://api.escuelajs.co/api/v1/products')
+  .then(res => res.json())
+  .then(data => {
+    obj = data;
+  })
+
+  /* CATEGORY ID 4 IS THE SHOES CATEGORY, THE ENTIRE SITE CATEGORY CAN BE CHANGED WITH THIS NUMBER */
+  .then(() => {
+    let newData = [];
+    for (let y = 0; y < data.length; y++){
+      if (data[y].category.id === 4) {
+        newData.push(data[y]);
+      }
+    }
+
+    /* THE API IS CALLED AND THE FIRST 11 PRODUCTS OF THE SHOE COLLECTION ARE LOADED INTO THE FIRST COLLECTION CAROUSEL */
+    for (let i = 0; i < 11; i++) {
+      if (newData[i].images != '') {
+      let prod = document.createElement('div');
+      prod.className = 'slide';
+        prod.style.backgroundImage = 'url(' + newData[i].images + ')';
+        let cover = document.createElement('div');
+        cover.className = 'slide-cover';
+        let span = document.createElement('span');
+        span.textContent = '$' + newData[i].price;
+        span.className = 'cover-copy';
+        cover.appendChild(span);
+        prod.appendChild(cover);
+        firstCollectionViewport.appendChild(prod);
+      }
+    }
+
+    /* THE API IS CALLED AND THE NEXT 11 PRODUCTS OF THE SHOE COLLECTION ARE LOADED INTO THE SECOND COLLECTION CAROUSEL */
+    for (let i = 11; i < 22; i++) {
+      if (newData[i].images != '') {
+      let prod = document.createElement('div');
+      prod.className = 'slide';
+        prod.style.backgroundImage = 'url(' + newData[i].images + ')';
+      let cover = document.createElement('div');
+      cover.className = 'slide-cover';
+      let span = document.createElement('span');
+      span.textContent = '$' + newData[i].price;
+       span.className = 'cover-copy';
+      cover.appendChild(span);
+      prod.appendChild(cover);
+      secondCollectionViewport.appendChild(prod);
+      }
+    }
+  });
+
+  //FEATURED PRODUCT API CALL
+  fetch('https://api.escuelajs.co/api/v1/products')
+    .then(res => res.json())
+    .then(data => {
+      obj = data;
+    })
+    .then(() => {
+      let thisData = [];
+      for (let y = 0; y < data.length; y++) {
+        if (data[y].category.id === 4) {
+          thisData.push(data[y]);
+        }
+      }
+
+      /* THE FEATURED PRODUCT SECTION HAS A SINGLE PRODUCT PULLED AND THE SECTION'S CONTENT IS POPULATED FROM THIS PRODUCT'S DATA. */
+      let featuredImage = document.getElementById('image-block');
+      let featuredCopy = document.getElementById('copy-block');
+
+      let featuredHeader = document.createElement('h2');
+      featuredHeader.textContent = 'Speed Run 2';
+      let featuredPrice = document.createElement('h4');
+      featuredPrice.textContent = '$' + thisData[30].price;
+      let featuredBody = document.createElement('p');
+      featuredBody.textContent = 'The Speed Run 2 has the same supersoft feel that lets you feel the potential when your foot hits the pavement. We created the shoe with plenty of snappy responsiveness and incredible support to help keep you feeling secure and competitive. Its 1 of our most tested shoes, still designed for you to stay on the track and away from the sidelines.';
+      let featuredButton = document.createElement('button');
+      featuredButton.className = 'button';
+      featuredButton.textContent = 'Add to Cart';
+
+      featuredCopy.appendChild(featuredHeader);
+      featuredCopy.appendChild(featuredPrice);
+      featuredCopy.appendChild(featuredBody);
+      featuredCopy.appendChild(featuredButton);
+    });
